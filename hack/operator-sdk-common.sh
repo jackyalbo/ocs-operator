f17b48819b3300c7a283a5ce3228670fd663924d
jackyalbo <jalbo@redhat.com>
2020-04-27 13:49:28 +0300

    updating rook version to 1.3.2
    Signed-off-by: jackyalbo jalbo@redhat.com

--- hack/operator-sdk-common.sh
+++ hack/operator-sdk-common.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OPERATOR_SDK_URL="${OPERATOR_SDK_URL:-https://github.com/operator-framework/operator-sdk/releases/download}"
-OPERATOR_SDK_VERSION="${OPERATOR_SDK_VERSION:-v0.16.0}"
+OPERATOR_SDK_VERSION="${OPERATOR_SDK_VERSION:-v0.17.0}"
 OPERATOR_SDK_PLATFORM="x86_64-linux-gnu"
 OS_TYPE=$(uname)
 if [ "$OS_TYPE" == "Darwin" ]; then
